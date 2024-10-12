1e533f982afb507114a93c4e31483a62b22ba734
AndreasHiropedi <s2015345@ed.ac.uk>
2024-10-13 02:51:41 +0400

    somehow cost is now 20

--- solution.py
+++ solution.py
@@ -55,7 +55,8 @@ class Model(object):
 
         predictions = gp_mean
         for i in range(0, len(gp_std)):  # if its residential add the standard deviation
-            if test_area_flags[i] == 1:
+            # Since flags are stored as booleans, just changed this check to 'Check if True'
+            if test_area_flags[i]:
                 predictions[i] += gp_std[i]
 
         # TODO: Use the GP posterior to form your predictions here
@@ -76,7 +77,8 @@ class Model(object):
         x_feat_0 = []
         train_targets_0 = []
         for i in range(0, len(x_feat) - 1):
-            if train_area_flags[i] == 1:
+            # Since flags are stored as booleans, just changed this check to 'Check if True'
+            if train_area_flags[i]:
                 x_feat_1.append(x_feat[i])
                 train_targets_1.append(train_targets[i])
             else:
@@ -96,9 +98,11 @@ class Model(object):
 
         x_feat = x_feat_temp
         train_targets = train_targets_temp
-        # kernel = ConstantKernel() + RationalQuadratic(alpha=1e+05, length_scale=0.184) + ExpSineSquared(length_scale=0.538, periodicity=1.1) + DotProduct(sigma_0=29.3) + RBF(length_scale=1e+05, length_scale_bounds=(1e4, 1e6)) + Matern(length_scale=4.51e-05, nu=1.5) + WhiteKernel(noise_level=225)
-        kernel = ConstantKernel() + RationalQuadratic(alpha=1e+05, length_scale=0.0852) + DotProduct(
-            sigma_0=23.3) + RBF(length_scale=1e+05, length_scale_bounds=(1e4, 1e6)) + WhiteKernel(noise_level=196)
+
+        # New kernel formula
+        kernel = ConstantKernel() * RationalQuadratic(alpha=1e+04, length_scale=0.0761) + DotProduct(
+            sigma_0=23.3) + RBF(length_scale=1e+04) + WhiteKernel(noise_level=177)
+
         self.model = GaussianProcessRegressor(kernel).fit(y=train_targets, X=x_feat)
         print(self.model.kernel_)
 
